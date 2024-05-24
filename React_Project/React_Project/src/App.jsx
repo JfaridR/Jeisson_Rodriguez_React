a1cac918e8124d0f2da3486533b7115940883381
JfaridR <jfaridrodriguezl@gmail.com>
2024-05-24 15:51:01 -0500

    Tarea2 - Proyecto React v1

---
+++ React_Project/React_Project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Avatar by James Cameron</h1>
+      <img src="https://cdn.mos.cms.futurecdn.net/ifZxgpjqTS8Dp7AuozUcSG-1200-80.jpg" alt="Foto pelicula" />
+      <p>
+        Avatar es una pelìcula dirigida por James Cameron y que fue estrenada en el 
+        año 2009 luego de más de 10 años de producción. Avatar es una película de 
+        ficción que muestra la vida en otro planeta donde habitan seres muy parecidos 
+        a los humanos y que verán afectada su paz y armonía debido a la presencia de 
+        los humanos provenientes de la tierra. La película utiliza un concepto muy 
+        llamativo el cual permite clonar la mente de una persona y poder trasladarlos
+        a un cuerpo totalmente diferente, tecnología usada principalmente para guerra
+        y que permitirá una mejor interacción con estos habitantes del planeta Navy.
+      </p>
+    </div>
+  );
+}
+
+export default App;
